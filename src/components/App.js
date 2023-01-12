50868ffb4d905368fdd5a389c0ddc3ccfd5492d4
RahulSanodia <r7sanodia@gmail.com>
2023-01-12 14:16:35 +0530

    1St  push

--- src/components/App.js
+++ src/components/App.js
@@ -17,14 +17,36 @@ var colors = [
 ];
 
 const App = () => {
-
+  const [quote,setQuaote]= React.useState(null);
+  const  getQuote =()=>{
+    fetch("https://api.quotable.io/random")
+    .then((response)=>response.json())
+    .then((data)=>setQuaote(data));
+    document.querySelector('body').style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+  }
+  React.useEffect(()=>getQuote(),[]);
+if(quote)
     return (
       <div id="main">
         <div id="wrapper">
-          
+        <div  id="quote-box">
+          <div className="quote-text">
+            <i  className=" fa fa-quote-left"></i>
+            <span id="text">{quote.content}</span>
+
+          </div>
+          <div  className="quote-author">
+            -<span id="author">{quote.author}</span>
+          </div>
+
+          <div   className="buttons">
+            <button className="button" id="new-quote" onClick={getQuote}>New Quote</button>
+          </div>
+          </div>  
         </div>
       </div>
     );
+    return <div> Loading....</div>
 };
 
 export default App;
